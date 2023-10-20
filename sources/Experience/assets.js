3c0f8118714990ec627235fc97602d7db705d55f
Lucien <lucien.boisseau-sable@smice.com>
2023-10-20 11:07:19 +0200

    init(project)

---
+++ sources/Experience/assets.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: 'base',
+    data: {},
+    items: [
+    //   { name: 'lennaTexture', source: '/assets/lenna.png', type: 'texture' },
+    //   { name: 'matcap', source: '/assets/matcap.png', type: 'texture' },
+      { name: 'carapuce', source: '/assets/carapuce.png', type: 'texture' },
+    //   { name: 'env', source: '/assets/sunset.exr', type: 'exr' },
+    //   { name: 'caillou', source: '/assets/caillou.glb' },
+    //   { name: 'robot', source: '/assets/robot.glb' },
+    ],
+  },
+];
