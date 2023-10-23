e76916d0069cf33e52f3ed2615ed624f43580121
victorcngo <hello@victorcango.fr>
2023-10-23 14:18:20 +0200

    feat(World): call city.update

--- sources/Experience/World.js
+++ sources/Experience/World.js
@@ -1,44 +1,52 @@
-import { AmbientLight, DirectionalLight, DirectionalLightHelper } from 'three';
-import Experience from './Experience.js';
-import Terminal from './Components/Terminal.js';
-import Floor from './Components/Floor.js';
+import { AmbientLight, DirectionalLight, DirectionalLightHelper } from 'three'
+import Experience from './Experience.js'
+import Terminal from './Components/Terminal.js'
+import Floor from './Components/Floor.js'
+import City from './Components/City.js'
 
 export default class World {
-  constructor(_options) {
-    this.experience = new Experience();
-    this.config = this.experience.config;
-    this.scene = this.experience.scene;
-    this.resources = this.experience.resources;
+    constructor(_options) {
+        this.experience = new Experience()
+        this.config = this.experience.config
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
 
-    this.resources.on('groupEnd', (_group) => {
-      if (_group.name === 'base') {
-        this.init();
-      }
-    });
-  }
+        this.resources.on('groupEnd', (_group) => {
+            if (_group.name === 'base') {
+                this.init()
+            }
+        })
+    }
 
-  init() {
-    this.ambientLight = new AmbientLight(0xaaffaa, 0.5);
-    this.scene.add(this.ambientLight)
+    init() {
+        this.ambientLight = new AmbientLight(0xaaffaa, 0.5)
+        this.scene.add(this.ambientLight)
 
-    this.directionalLight = new DirectionalLight(0xffffff, 5);
-    this.directionalLight.position.set(0, 5, 5);
+        this.directionalLight = new DirectionalLight(0xffffff, 5)
+        this.directionalLight.position.set(0, 5, 5)
 
-    this.scene.add(this.directionalLight);
+        this.scene.add(this.directionalLight)
 
-    this.terminal = new Terminal();
-    this.floor = new Floor();
+        this.city = new City({
+            group: this.scene,
+        })
+        // this.terminal = new Terminal();
+        // this.floor = new Floor();
 
-    this.elapsedTime = 0;
-  }
+        this.elapsedTime = 0
+    }
 
-  resize() {}
+    resize() {}
 
-  update() {
-    this.deltaTime = this.time - window.performance.now();
-    this.elapsedTime = window.performance.now() * 0.001;
-    this.time = window.performance.now();
-  }
+    update() {
+        this.deltaTime = this.time - window.performance.now()
+        this.elapsedTime = window.performance.now() * 0.001
+        this.time = window.performance.now()
 
-  destroy() {}
+        if (this.city) {
+            this.city.update()
+        }
+    }
+
+    destroy() {}
 }
