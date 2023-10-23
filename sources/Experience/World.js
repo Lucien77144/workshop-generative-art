70be3628ac9a933a72b183baae6942dc37a6328b
victorcngo <hello@victorcango.fr>
2023-10-23 14:18:20 +0200

    feat(World): call city.update

--- sources/Experience/World.js
+++ sources/Experience/World.js
@@ -5,42 +5,46 @@ import Floor from './Components/Floor.js';
 import { LAYERS } from './Const/const.js';
 
 export default class World {
-  constructor(_options) {
-    this.experience = new Experience();
-    this.config = this.experience.config;
-    this.scene = this.experience.scene;
-    this.resources = this.experience.resources;
+    constructor(_options) {
+        this.experience = new Experience()
+        this.config = this.experience.config
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
 
-    this.resources.on('groupEnd', (_group) => {
-      if (_group.name === 'base') {
-        this.init();
-      }
-    });
-  }
+        this.resources.on('groupEnd', (_group) => {
+            if (_group.name === 'base') {
+                this.init()
+            }
+        })
+    }
 
   init() {
     this.ambientLight = new AmbientLight(0xaaffaa, 0.5);
     this.ambientLight.layers.set(LAYERS.ALL);
     this.scene.add(this.ambientLight);
 
-    this.directionalLight = new DirectionalLight(0xffffff, 5);
-    this.directionalLight.position.set(0, 5, 5);
+        this.directionalLight = new DirectionalLight(0xffffff, 5)
+        this.directionalLight.position.set(0, 5, 5)
 
-    this.scene.add(this.directionalLight);
+        this.scene.add(this.directionalLight)
 
-    this.terminal = new Terminal();
-    this.floor = new Floor();
+        this.terminal = new Terminal();
+        this.floor = new Floor();
 
-    this.elapsedTime = 0;
-  }
+        this.elapsedTime = 0
+    }
 
-  resize() {}
+    resize() {}
 
-  update() {
-    this.deltaTime = this.time - window.performance.now();
-    this.elapsedTime = window.performance.now() * 0.001;
-    this.time = window.performance.now();
-  }
+    update() {
+        this.deltaTime = this.time - window.performance.now()
+        this.elapsedTime = window.performance.now() * 0.001
+        this.time = window.performance.now()
 
-  destroy() {}
+        if (this.city) {
+            this.city.update()
+        }
+    }
+
+    destroy() {}
 }
