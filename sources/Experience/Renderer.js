a01149f1a12b74329e363bab1dc08f06fd1e0d48
victorcngo <hello@victorcango.fr>
2023-10-23 19:30:09 +0200

    test: setup City for generation (without stencil)

--- sources/Experience/Renderer.js
+++ sources/Experience/Renderer.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three'
 import Experience from './Experience.js'
-import { LAYERS } from './Const/const.js'
-import vertexShader from './Shaders/vertexShader.vert?raw'
-import fragmentShader from './Shaders/fragmentShader.frag?raw'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
+import { HalftonePass } from 'three/examples/jsm/postprocessing/HalftonePass.js'
 
 export default class Renderer {
     constructor(_options = {}) {
@@ -20,6 +20,8 @@ export default class Renderer {
             this.debugRendererFolder = this.debug.addFolder('Renderer')
             this.debugHalftoneFolder = this.debug.addFolder('Halftone')
         }
+
+        this.usePostprocess = true
 
         this.setInstance()
         this.setPostProcess()
@@ -60,11 +62,9 @@ export default class Renderer {
 
         // Debug
         if (this.debug) {
-            this.debugRendererFolder
-                .addColor(this, 'clearColor')
-                .onChange(() => {
-                    this.instance.setClearColor(this.clearColor)
-                })
+            this.debugRendererFolder.addColor(this, 'clearColor').onChange(() => {
+                this.instance.setClearColor(this.clearColor)
+            })
 
             this.debugRendererFolder
                 .add(this.instance, 'toneMapping', {
@@ -76,78 +76,79 @@ export default class Renderer {
                 })
                 .onChange(() => {
                     this.scene.traverse((_child) => {
-                        if (_child instanceof THREE.Mesh)
-                            _child.material.needsUpdate = true
+                        if (_child instanceof THREE.Mesh) _child.material.needsUpdate = true
                     })
                 })
 
-            this.debugRendererFolder
-                .add(this.instance, 'toneMappingExposure')
-                .min(0)
-                .max(10)
+            this.debugRendererFolder.add(this.instance, 'toneMappingExposure').min(0).max(10)
         }
     }
 
     setPostProcess() {
-        this.halftonePass = {
-            uniforms: {
-                shape: { value: 3 },
-                radius: { value: 100 },
-                rotateR: { value: 90 },
-                rotateB: { value: 90 },
-                rotateG: { value: 90 },
-                scatter: { value: 0 },
-                blendingMode: { value: 1 },
-                blending: { value: 1 },
-            },
-        }
+        this.postProcess = {}
+
+        /**
+         * Render pass
+         */
+        this.postProcess.renderPass = new RenderPass(this.scene, this.camera.instance)
+        this.postProcess.halftonePass = new HalftonePass()
+
+        // TODO - Try to pass params to the instance directly
+        this.postProcess.halftonePass.uniforms.shape.value = 3
+        this.postProcess.halftonePass.uniforms.radius.value = 100
+        this.postProcess.halftonePass.uniforms.rotateR.value = 90
+        this.postProcess.halftonePass.uniforms.rotateB.value = 90
+        this.postProcess.halftonePass.uniforms.rotateG.value = 90
+        this.postProcess.halftonePass.uniforms.scatter.value = 0
+        this.postProcess.halftonePass.uniforms.blendingMode.value = 1
+        this.postProcess.halftonePass.uniforms.blending.value = 1
 
         // Debug
         if (this.debug) {
             this.debugHalftoneFolder
-                .add(this.halftonePass.uniforms.shape, 'value')
+                .add(this.postProcess.halftonePass.uniforms.shape, 'value')
                 .min(1)
                 .max(5)
                 .step(1)
                 .name('shape')
             this.debugHalftoneFolder
-                .add(this.halftonePass.uniforms.radius, 'value')
+                .add(this.postProcess.halftonePass.uniforms.radius, 'value')
                 .min(0)
                 .max(100)
                 .step(1)
                 .name('radius')
             this.debugHalftoneFolder
-                .add(this.halftonePass.uniforms.rotateR, 'value')
+                .add(this.postProcess.halftonePass.uniforms.rotateR, 'value')
                 .min(0)
                 .max(180)
                 .step(1)
                 .name('rotateR')
             this.debugHalftoneFolder
-                .add(this.halftonePass.uniforms.rotateB, 'value')
+                .add(this.postProcess.halftonePass.uniforms.rotateB, 'value')
                 .min(0)
                 .max(180)
                 .step(1)
                 .name('rotateB')
             this.debugHalftoneFolder
-                .add(this.halftonePass.uniforms.rotateG, 'value')
+                .add(this.postProcess.halftonePass.uniforms.rotateG, 'value')
                 .min(0)
                 .max(180)
                 .step(1)
                 .name('rotateG')
             this.debugHalftoneFolder
-                .add(this.halftonePass.uniforms.scatter, 'value')
+                .add(this.postProcess.halftonePass.uniforms.scatter, 'value')
                 .min(0)
                 .max(1)
                 .step(0.01)
                 .name('scatter')
             this.debugHalftoneFolder
-                .add(this.halftonePass.uniforms.blendingMode, 'value')
+                .add(this.postProcess.halftonePass.uniforms.blendingMode, 'value')
                 .min(0)
                 .max(1)
                 .step(0.01)
                 .name('blendingMode')
             this.debugHalftoneFolder
-                .add(this.halftonePass.uniforms.blending, 'value')
+                .add(this.postProcess.halftonePass.uniforms.blending, 'value')
                 .min(0)
                 .max(5)
                 .step(1)
@@ -157,21 +158,17 @@ export default class Renderer {
         /**
          * Effect composer
          */
-        this.rt0 = new THREE.WebGLRenderTarget(
-            this.config.width,
-            this.config.height,
-            {
-                generateMipmaps: false,
-                minFilter: THREE.LinearFilter,
-                magFilter: THREE.LinearFilter,
-                format: THREE.RGBAFormat,
-                encoding: THREE.sRGBEncoding,
-                samples: 1,
-                stencilBuffer: true,
-            }
-        )
-        this.rt1 = this.rt0.clone();
-        this.rt2 = this.rt0.clone();
+        this.renderTarget = new THREE.WebGLRenderTarget(this.config.width, this.config.height, {
+            generateMipmaps: false,
+            minFilter: THREE.LinearFilter,
+            magFilter: THREE.LinearFilter,
+            format: THREE.RGBAFormat,
+            encoding: THREE.sRGBEncoding,
+            samples: 1,
+        })
+        this.postProcess.composer = new EffectComposer(this.instance, this.renderTarget)
+        this.postProcess.composer.setSize(this.config.width, this.config.height)
+        this.postProcess.composer.setPixelRatio(this.config.pixelRatio)
 
         this.renderMesh = new THREE.Mesh(
             new THREE.PlaneGeometry(2, 2),
@@ -198,46 +195,10 @@ export default class Renderer {
         // Instance
         this.instance.setSize(this.config.width, this.config.height)
         this.instance.setPixelRatio(this.config.pixelRatio)
-    }
 
-    renderTargets() {
-        this.scene = this.experience.scene;
-
-        // Base
-        this.scene?.traverse((o) => {
-            if (o.material?.name == 'TerminalMaterial') {
-                o.material.colorWrite = true;
-            }
-        })
-        this.experience.world?.terminal?.screenStencil.setMaterial('baseMat')
-        this.camera.instance.layers.disableAll()
-        this.camera.instance.layers.enable(LAYERS.GLOBAL)
-        this.instance.setRenderTarget(this.rt0)
-        this.instance.render(this.scene, this.camera.instance)
-
-        // Content
-        this.camera.instance.layers.disableAll()
-        this.camera.instance.layers.enable(LAYERS.GLOBAL)
-        this.camera.instance.layers.enable(LAYERS.SCREEN)
-        this.instance.setRenderTarget(this.rt1)
-        this.instance.render(this.scene, this.camera.instance)
-        
-        // Mask
-        this.experience.world?.terminal?.screenStencil.setMaterial('maskMat')
-        this.scene?.traverse((o) => {
-            if (o.material?.name == 'TerminalMaterial') {
-                o.material.colorWrite = false;
-            }
-        })
-        this.camera.instance.layers.disableAll()
-        this.camera.instance.layers.enable(LAYERS.GLOBAL)
-        this.instance.setRenderTarget(this.rt2)
-        this.instance.render(this.scene, this.camera.instance)
-
-        // Shader
-        this.camera.instance.layers.set(LAYERS.DEFAULT)
-        this.instance.setRenderTarget(null)
-        this.instance.render(this.renderMesh, this.camera.instance)
+        // Post process
+        this.postProcess.composer.setSize(this.config.width, this.config.height)
+        this.postProcess.composer.setPixelRatio(this.config.pixelRatio)
     }
 
     update() {
@@ -245,11 +206,15 @@ export default class Renderer {
             this.stats.beforeRender()
         }
 
-        this.renderTargets()
+        if (this.usePostprocess) {
+            this.postProcess.composer.render()
 
-        // Animate halftone
-        if (this.halftonePass.uniforms.radius.value > 10) {
-            this.halftonePass.uniforms.radius.value -= 1
+            // Animate halftone
+            if (this.postProcess.halftonePass.uniforms.radius.value > 7) {
+                this.postProcess.halftonePass.uniforms.radius.value -= 1
+            }
+        } else {
+            this.instance.render(this.scene, this.camera.instance)
         }
 
         if (this.stats) {
@@ -260,8 +225,8 @@ export default class Renderer {
     destroy() {
         this.instance.renderLists.dispose()
         this.instance.dispose()
-        this.rt0.dispose()
-        this.rt1.dispose()
-        this.rt2.dispose()
+        this.renderTarget.dispose()
+        this.postProcess.composer.renderTarget1.dispose()
+        this.postProcess.composer.renderTarget2.dispose()
     }
 }
