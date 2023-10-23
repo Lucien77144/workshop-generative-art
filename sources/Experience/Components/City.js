26fd51d5097ccf4b87f3c3ad20cb2bfd4f805ead
victorcngo <hello@victorcango.fr>
2023-10-23 14:13:23 +0200

    feat: play cell-fracture anim

--- sources/Experience/Components/City.js
+++ sources/Experience/Components/City.js
@@ -1,35 +1,53 @@
-import Experience from '../Experience';
-import { AnimationMixer } from 'three';
+import Experience from '../Experience'
+import { AnimationMixer } from 'three'
 
 export default class City {
-  constructor(_options) {
-    this.experience = new Experience();
-    this.config = this.experience.config;
-    this.group = _options.group;
-    this.resources = this.experience.resources;
+    constructor(_options) {
+        this.experience = new Experience()
+        this.config = this.experience.config
+        this.group = _options.group
+        this.resources = this.experience.resources
+        this.scene = this.resources.items.city.scene
+        this.animations = this.resources.items.city.animations
 
-    this.init();
-  }
+        this.init()
+        this.setAnimations()
+    }
 
-  init() {
-    this.city = this.resources.items.city.scene;
-    console.log(this.city);
-    // this.mixer = new AnimationMixer(this.city);
+    init() {
+        this.scene.position.set(0, -1, 0)
+        this.scene.scale.set(0.5, 0.5, 0.5)
 
-    this.city.position.set(0, -1, 0);
-    this.city.scale.set(0.5, 0.5, 0.5);
+        this.group.add(this.scene)
+    }
 
-    this.group.add(this.city);
-    // this.elapsedTime = 0;
-  }
+    setAnimations() {
+        if (this.scene.visible) {
+            this.mixer = new AnimationMixer(this.scene)
 
-  resize() {}
+            setTimeout(() => {
+                this.fireAnimation()
+            }, 3000)
+        }
+    }
 
-  update() {
-    // this.deltaTime = this.time - window.performance.now();
-    // this.elapsedTime = window.performance.now() * 0.001;
-    // this.time = window.performance.now();
-  }
+    fireAnimation() {
+        for (const animation of this.animations) {
+            const action = this.mixer.clipAction(animation)
+            action.clampWhenFinished = true
+            action.repetitions = 0
+            action.play()
+        }
+    }
 
-  destroy() {}
+    resize() {}
+
+    update() {
+        if (this.mixer) {
+            this.mixer.update(this.experience.time.delta * 0.0005)
+            console.log(this.experience.time.delta)
+        }
+    }
+
+    destroy() {}
 }
