9005d92b6bc05d806b4597c3caa096e65c1cfdab
victorcngo <hello@victorcango.fr>
2023-10-26 01:21:53 +0200

    chore: clear debug

--- sources/Experience/World.js
+++ sources/Experience/World.js
@@ -38,76 +38,14 @@ export default class World {
         this.directionalLight.layers.set(LAYERS.SCREEN)
         this.scene.add(this.directionalLight)
 
-        this.spotLight = new SpotLight(0xFFFDD1, 100)
+        this.spotLight = new SpotLight(0xfffdd1, 100)
         this.spotLight.position.set(0, 4, 0)
         this.spotLight.layers.set(LAYERS.DEFAULT)
         this.spotLight.angle = 0.528
         this.spotLight.penumbra = 0.2
         this.spotLight.decay = 2.8
         this.spotLight.distance = 0
-        // this.spotLightHelper = new SpotLightHelper(this.spotLight)
-        // this.scene.add(this.spotLightHelper)
         this.scene.add(this.spotLight)
-
-        // add debug for all options of spotlight
-        if (this.debug) {
-            console.log('debug')
-
-            this.debugSpotLightFolder = this.debug.addFolder('SpotLight')
-            // add deug for position x y z
-            this.debugSpotLightFolder
-                .add(this.spotLight.position, 'x')
-                .min(-10)
-                .max(10)
-                .step(0.1)
-                .onChange(() => {
-                    this.spotLightHelper.update()
-                })
-            this.debugSpotLightFolder
-                .add(this.spotLight.position, 'y')
-                .min(0)
-                .max(10)
-                .step(0.1)
-                .onChange(() => {
-                    this.spotLightHelper.update()
-                })
-            this.debugSpotLightFolder
-                .add(this.spotLight.position, 'z')
-                .min(-10)
-                .max(10)
-                .step(0.1)
-                .onChange(() => {
-                    this.spotLightHelper.update()
-                })
-            this.debugSpotLightFolder
-                .add(this.spotLight, 'intensity')
-                .min(0)
-                .max(1000)
-                .step(0.01)
-            this.debugSpotLightFolder
-                .add(this.spotLight, 'angle')
-                .min(0)
-                .max(Math.PI / 2)
-                .step(0.01)
-                .onChange(() => {
-                    this.spotLightHelper.update()
-                })
-            this.debugSpotLightFolder
-                .add(this.spotLight, 'penumbra')
-                .min(0)
-                .max(1)
-                .step(0.01)
-            this.debugSpotLightFolder
-                .add(this.spotLight, 'decay')
-                .min(0)
-                .max(10)
-                .step(0.01)
-            this.debugSpotLightFolder
-                .add(this.spotLight, 'distance')
-                .min(0)
-                .max(40)
-                .step(0.01)
-        }
 
         this.terminal = new Terminal()
         this.floor = new Floor()
